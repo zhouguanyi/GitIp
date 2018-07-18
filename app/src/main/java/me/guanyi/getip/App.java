a9673aa1875f99f3e964f74012bb9ee9ef52aef7
GuanYi <878901655@qq.com>
2018-07-18 10:30:04 +0800

    gitIp v0.0.1

---
+++ app/src/main/java/me/guanyi/getip/App.java
@@ -0,0 +1,18 @@
+package me.guanyi.getip;
+
+import android.app.Application;
+
+
+/**
+ * Created by ajf-dell on 2018/7/18.
+ */
+
+public class App extends Application {
+    private static final String TAG = "App";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/me/guanyi/getip/MainActivity.java
@@ -0,0 +1,84 @@
+package me.guanyi.getip;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.id_ip);
+
+        if (getAPNType(MainActivity.this) == 1){
+            try {
+                textView.setText(getPhoneIp());
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }else {
+            textView.setText("未连接WIFI");
+        }
+
+    }
+
+    /**
+     * 获取当前的网络状态 ：没有网络0：WIFI网络1：3G网络2：2G网络3
+     *
+     * @param context
+     * @return
+     */
+    public static int getAPNType(Context context) {
+        int netType = 0;
+        ConnectivityManager connMgr = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo == null) {
+            return netType;
+        }
+        int nType = networkInfo.getType();
+        if (nType == ConnectivityManager.TYPE_WIFI) {
+            netType = 1;// wifi
+        } else if (nType == ConnectivityManager.TYPE_MOBILE) {
+            int nSubType = networkInfo.getSubtype();
+            TelephonyManager mTelephony = (TelephonyManager) context
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+            if (nSubType == TelephonyManager.NETWORK_TYPE_UMTS
+                    && !mTelephony.isNetworkRoaming()) {
+                netType = 2;// 3G
+            } else {
+                netType = 3;// 2G
+            }
+        }
+        return netType;
+    }
+
+    public static String getPhoneIp() throws SocketException {
+        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+            NetworkInterface intf = en.nextElement();
+            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                InetAddress inetAddress = enumIpAddr.nextElement();
+                if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                    return inetAddress.getHostAddress().toString();
+                }
+            }
+        }
+        return "error";
+    }
+
+}
